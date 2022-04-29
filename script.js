707bac2f0b9a885b938590349a77f39172fb8ce7
Leonardo Zanette <leonardobzanette@gmail.com>
2022-04-29 12:26:50 -0300

    Requisito 12 - DONE

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ function adicionaCor() {
 }
 adicionaCor()
 
+function changeColors() {
+    let paleta = document.getElementsByClassName('color');
+    for(i = 1; i <paleta.length; i+=1) {
+        let cor = geraCores()
+        paleta[i].style.backgroundColor = cor
+    }
+}
+changeColors()
+
+
+function geraCores() {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    let rgb = 'rgb('+r+','+g+','+b+')'
+    return rgb;
+}
+
+
+
+
+
+
 
 
 function chamaChange() {
